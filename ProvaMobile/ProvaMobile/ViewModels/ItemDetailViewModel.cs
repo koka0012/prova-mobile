fb4b6ee25a4f1e19d2477d521a0ebb95587b2b2c
unknown <Administrador@LAB3-12.laboratorios.fasipe>
2018-12-04 19:26:16 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaMobile/ProvaMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ProvaMobile.Models;
+
+namespace ProvaMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
